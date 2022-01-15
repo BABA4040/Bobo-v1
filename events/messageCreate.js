dcc8645440fab0d6c8397452ec6b30222d4f1d40
Glitch (patch-dog-watercress) <none>
2022-01-15 08:42:41 +0000

    💁💎 Checkpoint ./events/messageCreate.js:24343700/2576

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -48,7 +48,7 @@ if (!user || !user.xp){
 
 
 ////--------xp system------/////*
-   hama(message)
+   /*hama(message)
     async function hama(message) {
 
       if (guild) {
@@ -57,8 +57,8 @@ if (!user || !user.xp){
         
 //      let xp = await Guild.collections.get('xp', message.guild.id);
 
-      /**  const max = 100;
-        const min = 1;*/
+        const max = 100;
+        const min = 1;
         const max = 250;
         const min = 10;
 
@@ -154,7 +154,92 @@ if (!user || !user.xp){
             return { xpAdded: false, reason: "DB_ERROR_ON_SAVE" };
           });
       }}
+*/
+        
+        xp(message)
+        async function xp(message){
+            const max = 25;
+  const min = 10;
+  const points = Math.floor(Math.random() * (max-min)) + min;
 
+
+  let res = await User.findOne({ userID: message.author.id }) ||
+  await new User({ userID: message.author.id }).save();
+
+  // Check if the data returned is a valid
+  if (!(res instanceof User)){
+    return promise.resolve({ xpAdded: false, reason: 'DB_ERROR'})
+  };
+
+  /*=======================CALCULATE XP============================*/
+  // Some weird math goes here...
+
+  // Get the server data
+  let index = res.data.xp.findIndex(x => x.id === message.guild.id);
+  let serverdata;
+
+  // Add serverdata to profile if it doesn't exist yet
+  // -1 means the index couldn't be found
+  if (index === -1){
+    res.data.xp.push({
+      id: message.guild.id,
+      xp: 0,
+      level: 1
+    });
+    index = res.data.xp.findIndex(x => x.id === message.guild.id);
+    [ serverdata ] = res.data.xp.splice(index,1);
+  } else {
+    [ serverdata ] = res.data.xp.splice(index,1);
+  };
+
+  // define points
+  let _xp = {
+    global: {
+      get cap(){ return (50 * Math.pow(res.data.global_level,2)) + (250 * res.data.global_level); },
+      get next(){ return this.cap -  res.data.global_xp; },
+    },
+    local: {
+      get cap(){ return (50 * Math.pow(serverdata.level,2)) + (250 * serverdata.level); },
+      get next(){ return this.cap - serverdata.xp }
+    }
+  };
+
+  // PROCESS GLOBAL XP
+  // Add 3xp xp add on global based xp
+  // Increment level if next is less than the current xp
+  res.data.global_xp = res.data.global_xp + 3;
+  while (_xp.global.next < 1){
+    res.data.global_level++
+  };
+
+  // PROCESS LOCAL XP
+  // Add points which was previously randomized on server[local] based xp
+  // increment level if next is less than the current xp.
+  serverdata.xp = serverdata.xp + points;
+  while (_xp.local.next < 1){
+    serverdata.level++
+  };
+
+  // Add xpdata again to the xp array of the profile
+  // index = where the serverdata is inserted
+  // 0 = number of elements to remove
+  // serverdata = the inserted data
+  res.data.xp.splice(index, 0, serverdata);
+
+  // Save the new data
+  return res.save()
+  .then(() => {
+    xp.set(message.author.id, {});
+    setTimeout(() => xp.delete(message.author.id), 60000);
+    return { xpAdded: true, reason: null };
+  })
+  .catch(() => {
+    return { xpAdded: false, reason: 'DB_ERROR_ON_SAVE' }
+  });}
+        
+        
+        
+        
     ///-----------////
 
 
