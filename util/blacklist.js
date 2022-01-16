218c2d2b8141197d7d57c2e65f6b1437c703bdfc
Glitch (patch-dog-watercress) <none>
2022-01-16 07:17:06 +0000

    🦁🍵 Checkpoint ./util/blacklist.js:24343700/858

--- util/blacklist.js
+++ util/blacklist.js
@@ -1,1 +1,19 @@
-async function black(
+async function black(bot, message){
+  
+
+   const userBlacklistSettings = await Black.findOne({ userID: message.author.id,});
+  const guildBlacklistSettings = await Black.findOne({ Guild: message.guild.id });
+
+
+
+      if (userBlacklistSettings && userBlacklistSettings.isBlacklisted) {
+       //   logger.warn(`${message.author.tag} tried to use "${cmd}" command but the user is blacklisted`, { label: 'Commands' })
+          return;// message.channel.send(`You are blacklisted from the bot :(`);
+        }
+
+        // Check if server is Blacklisted
+        if (guildBlacklistSettings && guildBlacklistSettings.isBlacklisted) {
+        //  logger.warn(`${message.author.tag} tried to use "${cmd}" command but the guild is blacklisted`, { label: 'Commands' })
+          return;//message.channel.send(` This guild is Blacklisted :(`);
+        }}
+module.exports = black
