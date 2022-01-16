89a21dee536d5b2dde5e16af3ed1ff26a9e006b7
Glitch (patch-dog-watercress) <none>
2022-01-16 23:03:13 +0000

    ☄️💕 Checkpoint ./events/guildCreate.js:24343700/122

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,9 +3,11 @@ module.exports = class{
   async run(bot, message,guild){
     
     const embed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+	.setTitle('Some title')
     .addField(`I joined a new guild`)
-    .addField(guild.memberCount)
-    .addFiled(guild.owner)
+    .addField(`hi ${message.guild.memberCount}`)
+    .addField(`hi<@${message.guild.ownerId}>`)
     log.send({embeds: [embed]});
     
   }}
