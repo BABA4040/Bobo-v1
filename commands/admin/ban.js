98a51343d393276d72a0fe70865371ed90dc9eed
Glitch (patch-dog-watercress) <none>
2022-01-17 18:15:39 +0000

    👨‍❤️‍👨🏥 Checkpoint ./commands/admin/ban.js:24343700/1433

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -15,7 +15,7 @@ module.exports = {
     run: async (client, message, args, dev) => {
   
      let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-
+let reason = args.slice(3).join('')
     if (!user)
       return message.channel.send({content:`Usage: ban [@User]`}).catch(console.error);
 
@@ -28,12 +28,21 @@ module.exports = {
     if (message.guild.ownerId !== message.author.id && user.roles.highest.comparePositionTo(message.member.roles.highest) >= 0)
       return message.channel.send({content:`Based on the role hierarchy, you cannot ban this user`});
 
-    if (!message.guild.member(user).bannable) return message.channel.send({content:`I cannot ban the mentioned user`});
-
+  //  if (!message.guild.members(user).bannable) return message.channel.send({content:`I cannot ban the mentioned user`});
+let Ww = await Owner.findOne({ ownerCode: "768944616724103170" });
   
-  
+  if (user.id === Ww.worldWhitelist.find((c) => c.type === message.author.id)) return await message.channel.send({content:`You can't ban my devlopers`});
 
-    message.channel.send({content:`**${user}** has been banned from server`});
-    user.ban({ reason: args[1] });
+
+
+      await user.send(`**${message.author.tag}** banned you from ${message.guild.name}!\n**Reason**: ${reason.join(' ') || 'Unspecified.'}`)
+    .catch(() => null);
+
+    return user.ban({ reason: `MAI Ban Command: ${message.author.tag}: ${reason.join(' ') || 'Unspecified'}`})
+    .then(_member => message.channel.send(`Successfully banned **${_member.user.tag}**`))
+    .catch(() => message.channel.send(`Failed to ban **${user.user.tag}**!`));
+
+    
+
   }
 }
