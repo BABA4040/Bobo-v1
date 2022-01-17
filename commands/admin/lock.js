ed8a75cc6938c1ef7bd3d73d55cc3c519d1c52f5
Glitch (patch-dog-watercress) <none>
2022-01-17 12:09:14 +0000

    🚙📖 Checkpoint ./commands/admin/lock.js:24343700/983

---
+++ commands/admin/lock.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "lock",
+  aliases: ["close","lock"],
+  description: "Locks the current or selected text channels",
+  usage: ["s!lock"],
+  category: ["admin"],
+  enabled: true,			  
+  memberPermissions: [ "MANAGE_CHANNELS" ],			
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_CHANNELS" ],		
+  ownerOnly: false,			
+  cooldown: 6000,
+  run: async (bot, message, args, dev, data) => {
+  
+  message.channel
+      .permissionOverwrites.edit(message.guild.id, {
+        SEND_MESSAGES: false
+      })
+      .then(() => {
+        message.channel.send({content:`channel locked`});
+     });
+   }
+}
