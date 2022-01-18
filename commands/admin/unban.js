a4657d06c215b761d994892a06765c7b9e06b366
Glitch (patch-dog-watercress) <none>
2022-01-18 14:16:33 +0000

    💒⛱ Checkpoint ./commands/admin/unban.js:24343700/2185

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -9,8 +9,8 @@ module.exports = {
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","BAN_MEMBERS" ],        
   ownerOnly: false,            
   cooldown: 10000,
-  run: async (bot, message, dev, data,args,[ user = '' ]) => {
-    if (!user.match(/\d{17,19}/)){
+  run: async (bot, message, dev, data,args) => {
+  /*  if (!user.match(/\d{17,19}/)){
       return message.channel.send({content:`${message.author}, Please provide the ID of the user to unban`});
     };
 
@@ -18,4 +18,47 @@ module.exports = {
 
     return message.guild.members.unban(user, { reason: `Mai Unbans: ${message.author.tag}: ${args.join(' ') || 'None'}`})
     .then(user => message.channel.send({content:`\\✔️ Successfully unbanned **${user.tag}**!`}))
-    .catch(() => message.channel.send({content:`\\❌ Unable to unban user with ID ${user}. The provided argument maybe not a valid user id, or the user is not banned.`}));}}
+    .catch(() => message.channel.send({content:`\\❌ Unable to unban user with ID ${user}. The provided argument maybe not a valid user id, or the user is not banned.`}));}}*/
+  
+    		let user = null;
+
+		if(!args[1]){
+			return message.channel.send({content:`Please Put Id of user`})
+		}
+
+		// Check if the arg is an ID or a username
+		const isId = !isNaN(args[1]);
+
+		if(isId){
+			// Try to find a user with that ID
+			await bot.users.fetch(args[1]).then((u) => {
+				// if a user was found
+				user = u;
+			}).catch(() => {});
+		} else if(!isId) {
+			const arr = args[1].split("#");
+			if(arr.length < 2){
+				return message.channel.send({content:`I can't found`})
+			}
+			user = bot.users.filter((u) => u.username === arr[0]).find((u) => u.discriminator === arr[1]);
+		}
+
+		if(!user){
+			return message.channel.send({content:`❌ | i cant find this user`}) ;
+		}
+
+		// check if the user is banned
+		const banned = await message.guild.bans.fetch();
+		if(!banned.some((e) => e.user.id === user.id)){
+			return message.channel.send({content:`I think This user **${user.tag}** not banned from server`})}
+		
+
+		// Unban user
+		message.guild.members.unban(user).catch(() => {});
+
+		// Send a success message in the current channel
+		message.channel.send({content:`${user.tag} unbanned by ${message.authlr.tag}`});
+
+	}
+    
+  }
