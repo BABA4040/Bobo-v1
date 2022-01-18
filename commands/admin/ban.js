9a60d557aef33999d4f9f41370523d9d2f019c52
Glitch (patch-dog-watercress) <none>
2022-01-18 15:49:29 +0000

    🎮🌖 Checkpoint ./commands/admin/ban.js:24343700/851

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const { Color } = require("../../config.js");
+const userReg = RegExp(/<@!?(\d+)>/)
 
 module.exports = {
     name: "ban",
@@ -12,16 +13,18 @@ module.exports = {
     botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
     ownerOnly: false,
     cooldown: 6000,
-    run: async (client, message, args, dev) => {
+    run: async (bot, message, args, dev) => {
   
-     let user = await message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-if(isNaN(user)){
+     let user = await message.mentions.members.first() || message.guild.members.cache.get(args[1])
+     let admin = message.author;
+     // if(!user) return message.channel.send({content:` Mention someone or put id`})
+if(userReg(user)){
   let reason = args.join('');
-      if(user.id = message.author.id){
+      if(user.id === admin.id){
         return message.channel.send({content:` you can't ban yourself`})
         
       }
-      if(user.id = client.user.id) {
+      if(user.id === bot.user.id) {
         
         return message.channel.send({content:`🙂 |You want ban me why....!`})
         
@@ -38,15 +41,24 @@ if(isNaN(user)){
     
 
   }
+      
     if(Number(user)){
-      if(user = message.author.id){
+      let reason = args.join('');
+      if(user === admin.id){
         return message.channel.send({content:`You cant ban yourself`})
       }
-      if(user = client.author.id){
+      if(user === bot.user.id){
         return message.channel.send({content:`🙂 | You want ban me by myself why...!`})
       }
       
       
+    await user.send(`**${message.author.tag}** banned you from ${message.guild.name}!\n**Reason**: ${reason || 'Unspecified.'}`)
+    .catch(() => null);
+
+    return user.ban({ reason: `Bobo Ban Command: ${message.author.tag}: ${reason || 'Unspecified'}`})
+    .then(_member => message.channel.send(`Successfully banned **${_member.user.tag}**`))
+    .catch(() => message.channel.send(`Failed to ban **${user.user.tag}**!`));
+
       
       
     }
