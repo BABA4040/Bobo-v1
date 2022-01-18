0e763d7232d3bee46ff3686c06ab8a493a3c65f8
Glitch (patch-dog-watercress) <none>
2022-01-18 14:40:27 +0000

    🍭🚗 Checkpoint ./commands/admin/ban.js:24343700/274

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -14,16 +14,16 @@ module.exports = {
     cooldown: 6000,
     run: async (client, message, args, dev) => {
   
-     let user = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+     let user = await message.mentions.members.first() || message.guild.members.cache.get(args[1]);
 let reason = args.slice(3).join('')
-    if (!user)
+  /*  if (!user)
       return message.channel.send({content:`Usage: ban [@User]`}).catch(console.error);
-
-    if (user.id === message.author.id) {
+*/
+    if (user === message.author.id) {
       return message.channel.send({content:`You can't ban yourself`});
     }
 
-    if (user.id === client.user.id) return message.channel.send({content:`I can't ban myself`});
+    if (user=== client.user.id) return message.channel.send({content:`I can't ban myself`});
 
     if (message.guild.ownerId !== message.author.id && user.roles.highest.comparePositionTo(message.member.roles.highest) >= 0)
       return message.channel.send({content:`Based on the role hierarchy, you cannot ban this user`});
@@ -31,7 +31,7 @@ let reason = args.slice(3).join('')
   //  if (!message.guild.members(user).bannable) return message.channel.send({content:`I cannot ban the mentioned user`});
 let Ww = await Owner.findOne({ ownerCode: "768944616724103170" });
   
-  if (user.id === Ww.ownerCode) return await message.channel.send({content:`You can't ban my devlopers`});
+  if (user === Ww.ownerCode) return await message.channel.send({content:`You can't ban my devlopers`});
 
 
 
