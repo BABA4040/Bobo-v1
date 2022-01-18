da5866c91dfdef00423ab608c355f38877b04337
Glitch (patch-dog-watercress) <none>
2022-01-18 16:01:29 +0000

    👳✋ Checkpoint ./commands/admin/ban.js:24343700/4048 ./commands/admin/kick.js:24343700/1700

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -3,14 +3,14 @@ const { Color } = require("../../config.js");
 const userReg = RegExp(/<@!?(\d+)>/)
 
 module.exports = {
-    name: "ban",
-    aliases: ["band","ban"],
-    description: "You can ban a member, or multiple members using this command",
-    usage: ["ban [@User]"],
+    name: "kick",
+    aliases: ["kick"],
+    description: "You can kick a member, using this command",
+    usage: ["kick [@User]"],
     category: ["admin"],
     enabled: true,
-    memberPermissions: ["BAN_MEMBERS"],
-    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
+    memberPermissions: ["KICK_MEMBERS"],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "KICK_MEMBERS"],
     ownerOnly: false,
     cooldown: 6000,
     run: async (bot, message, args, dev) => {
@@ -21,12 +21,12 @@ module.exports = {
 if(user){
   let reason = args.slice(3).join('');
       if(user.id === admin.id){
-        return message.channel.send({content:` you can't ban yourself`})
+        return message.channel.send({content:` you can't kick yourself`})
         
       }
       if(user.id === bot.user.id) {
         
-        return message.channel.send({content:`🙂 |You want ban me why....!`})
+        return message.channel.send({content:`🙂 |You want kick me why....!`})
         
       }
     
@@ -34,7 +34,7 @@ if(user){
       await user.send(`**${message.author.tag}** banned you from ${message.guild.name}!\n**Reason**: ${reason|| 'Unspecified.'}`)
     .catch(() => null);
 
-    return user.ban({ reason: `Ban Command: ${message.author.tag}: ${reason || 'Unspecified'}`})
+    return user.kick({ reason: `Ban Command: ${message.author.tag}: ${reason || 'Unspecified'}`})
     .then(_member => message.channel.send(`Successfully banned **${_member.user.tag}**`))
     .catch((err) => message.channel.send(`Failed to ban **${user.user.tag} : reason: Your role not high than this member or ${err.name}**!`));
 
