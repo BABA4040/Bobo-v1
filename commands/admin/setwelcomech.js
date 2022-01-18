e539a1aeb3cbe5664d2b5e97f17e99caa7af9115
Glitch (patch-dog-watercress) <none>
2022-01-18 19:23:45 +0000

    🐽✊ Checkpoint ./commands/admin/setwelcomech.js:24343700/734

--- commands/admin/setwelcomech.js
+++ commands/admin/setwelcomech.js
@@ -9,23 +9,27 @@ module.exports = {
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "MANAGE_CHANNELS"],
   ownerOnly: false,
   cooldown: 6000,
-  run: async (bot, message, args, dev, data) => {
+  run: async (bot, message, args, dev) => {
     
   let data = await Guild.findOneAndUpdate({guildID: message.guild.id});
   let channel = await message.mentions.channels.first() || message.guild.channels.fetch(args[1]);
     
     if(!channel) return message.channel.send({content:` Mention channel or Put id channel`})
     
-    
-    if(channel){
-      
-      
-      
-      
-      
-      
-    }
-    
-    
+  
+if(data){
+   
+data.channels.welcomech = channel.id;
+data.save();
+}
+          channel.permissionOverwrites.create(
+          channel.guild.roles.everyone,
+          {
+            SEND_MESSAGES: false,
+
+            VIEW_CHANNEL: true
+          }
+        )
+    message.channel.send({content:`welcome channel setuped use Botest`})
     
   }}
