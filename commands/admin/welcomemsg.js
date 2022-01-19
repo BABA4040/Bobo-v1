afcb40729394acca10735c9e94ba22d32fc1c9fb
Glitch (patch-dog-watercress) <none>
2022-01-19 15:02:31 +0000

    🌆🐊 Checkpoint ./commands/admin/welcomemsg.js:24343700/378

--- commands/admin/welcomemsg.js
+++ commands/admin/welcomemsg.js
@@ -9,12 +9,21 @@ module.exports = {
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "MANAGE_CHANNELS"],
   ownerOnly: false,
   cooldown: 6000,
-  run: async (bot, message, args, dev, data) => {
+  run: async (bot, message, args, dev) => {
     
     let data = await Guild.findOneAndUpdate({guildID: message.guild.id})
     
     
+    if(args[1] === "channel"){
+      let channel = await message.mentions.channels.first();
+      if(!channel) return message.channel.send({content:`Mention channel please`})
+      
+      message.guild.channels.fetch(channel).then(ch =>{
+        if(!ch) 
     
+    
+    
+    }
     
     
   }
