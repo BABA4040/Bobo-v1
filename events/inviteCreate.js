187d236cea466d109fe520696b6dd4e72e24f0c4
Glitch (patch-dog-watercress) <none>
2022-01-21 08:14:25 +0000

    🌛🛀 Checkpoint ./events/guildCreate.js:24343700/1997 ./config.json:24343700/5510

--- events/inviteCreate.js
+++
@@ -1,9 +0,0 @@
-//onst { Listener } = require('discord-akairo');
-module.exports = class {
-  
-    async run(invite,bot) {
-        let invites = await bot.guild.invents.fetch();
-        if(invite.guild.vanityURLCode) invites.set(invite.guild.vanityURLCode, await invite.guild.fetchVanityData());
-        bot.guild.invites.set(invite.guild.id, invites);
-    };
-};
