b5da65762c6efb12486c1f897f15060e6f7ca158
Glitch (patch-dog-watercress) <none>
2022-01-21 13:11:13 +0000

    👾🌹 Checkpoint ./util/givebadge.js:24343700/986 ./events/messageCreate.js:24343700/2001

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -77,11 +77,26 @@ module.exports = class {
          
          
        }
-    const badge = require(`${process.cwd()}/util/givebadge`),
-          badges = await badge(message, bot);
-                          
+                   
+    
+
+    
+      if(user.data.command.uses = 1011){
+  
+    
+    user.badge = config.badge.lover;
+    user.save();
     
     
+  
+  message.channel.send({content:` you got new badge by the bot because you used bot 1000 times new badge${config.badge.lover}`})}
+
+  if(user.data.game.uses = 1000){
+    user.badge = config.badge.game;
+    user.save()
+  
+  
+}
      if (guild) {
        
       
