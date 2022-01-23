cd740dbe900665e0debe1261603f73d870dfb3fa
Glitch (patch-dog-watercress) <none>
2022-01-23 08:44:13 +0000

    🔋🏔 Checkpoint ./commands/admin/kick.js:24343700/172

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -3,14 +3,14 @@ const { Color } = require("../../config.js");
 
 
 module.exports = {
-    name: "ban",
-    aliases: ["band","ban"],
-    description: "You can ban a member, or multiple members using this command",
-    usage: ["ban [@User]"],
+    name: "kick",
+    aliases: ["kick"],
+    description: "kick",
+    usage: ["kick"],
     category: ["admin"],
     enabled: true,
-    memberPermissions: ["BAN_MEMBERS"],
-    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
+    memberPermissions: ["KICK_MEMBERS"],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "KICK_MEMBERS"],
     ownerOnly: false,
     cooldown: 6000,
     run: async (bot, message, args, dev) => {
@@ -30,15 +30,15 @@ if(member){
 				return message.channel.send({content:`You can't sanction or update a sanction for a member who has an higher or equal role hierarchy to yours!
     `})
 			}
-			if(!member.bannable) {
+			if(!member.kickable) {
 				return message.channel.send({content:`An error has occurred... Please check that I have the permission to ban this specific member and try again!`})
 			}
 		
       
-      await user.send(`**${message.author.tag}** banned you from ${message.guild.name}!\n**Reason**: ${reason|| 'Unspecified.'}`)
+      await user.send(`**${message.author.tag}**kicked you from ${message.guild.name}!\n**Reason**: ${reason|| 'Unspecified.'}`)
     .catch(() => null);
 
-    return user.ban({ reason: `Kick Command: ${message.author.tag}: ${reason || 'Unspecified'}`})
+    return user.kick({ reason: `Kick Command: ${message.author.tag}: ${reason || 'Unspecified'}`})
     .then(_member => message.channel.send(`Successfully Kicked **${_member.user.tag}**`))
     .catch((err) => message.channel.send(`Failed to ban **${user.user.tag} : reason: Your role not high than this member or ${err.name}**!`));
 
