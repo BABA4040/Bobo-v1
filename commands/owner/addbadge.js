79b65593e2888ad2ad1c05308170604bea544e80
Glitch (patch-dog-watercress) <none>
2022-01-24 14:20:30 +0000

    🌒🎀 Checkpoint ./commands/owner/addbadge.js:24343700/829

--- commands/owner/addbadge.js
+++ commands/owner/addbadge.js
@@ -1,26 +1,37 @@
+let badges = require(`${process.cwd()}/config.json`)
+
 module.exports = {
   name: "addbadge",
-  aliases: [],
-  description: "t",
-  usage: [],
+  aliases: [""],
+  description: "",
+  usage: [""],
   category: ["owner"],
   enabled: true,
-  memberPermissions: [],
+  memberPermissions: ["SEND_MESSAGES"],
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   ownerOnly: true,
   cooldown: 6000,
   run: async (bot, message, args, dev,prefix) => {
     
   
-let data = await Guild.findOneAndUpdate({guildID: message.guild.id})
+///let data = await Guild.findOneAndUpdate({guildID: message.guild.id})
 let user = await message.mentions.users.first() || await bot.users.cache.get(args[1])
 if(!user) return message.channel.send({content:`please mention any one to give badge`})
-    
+    let data= await User.findOneAndUpdate({userID: user.id})
 if(user){
-  let badges = config.badge
-  
+
+  let id = args[2];
+  if(!Number(id)) return message.channel.send({content:`Id required only number`})
   let badge = badges.find(x => x.id=== id)
   
+  const old = data.data.badge.find(x=> x.id=== config.badge.id)
+  if(old){ return message.channel.send({content:`this user have badge`})
+         }else{
+  data.data.badge.push({
+    id: badge.id,
+    type: badge.type})
+          return message.channel.send({content:` pushed to user`})
+           
   
   
-}}}
+}}}}
