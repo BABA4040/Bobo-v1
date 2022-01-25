ec5aeb7e0fde439a200decbe5a84327ccaf8ef8c
Glitch (patch-dog-watercress) <none>
2022-01-25 21:55:49 +0000

    🕵👯 Checkpoint ./handler/slash.js:24343700/3146

--- handler/slash.js
+++ handler/slash.js
@@ -9,41 +9,36 @@ const { Routes } = require("discord-api-types/v9");
 
 const rest = new REST({ version: "9" }).setToken(config.token);
 const guild = "850129663896190996";
-module.exports = async bot => {
+module.exports = async (bot) => {
   //////resting slash command
   const commands = [];
-  
-  
-  
+
   try {
     console.log("Started refreshing application (/) commands.");
-    
-    
-  if (!guild) {
-                await rest.put(
-                    Routes.applicationCommands(config.clientID), {
-                        body: commands
-                    },
-                );
-                console.log('Successfully registered application commands globally');
-            } else {
-                await rest.put(
-                    Routes.applicationGuildCommands(config.clientID, guild), {
-                        body: commands
-                    },
-                );
-                console.log('Successfully registered application commands for development guild');
-    
-    
-    console.log("Successfully reloaded application (/) commands.");
-  }} catch (error) {
+
+    if (!guild) {
+      await rest.put(Routes.applicationCommands(config.clientID), {
+        body: commands,
+      });
+      console.log("Successfully registered application commands globally");
+    } else {
+      await rest.put(Routes.applicationGuildCommands(config.clientID, guild), {
+        body: commands,
+      });
+      console.log(
+        "Successfully registered application commands for development guild"
+      );
+
+      console.log("Successfully reloaded application (/) commands.");
+    }
+  } catch (error) {
     console.log(error);
   }
 
-  fs.readdirSync("./C-slash/").forEach(dir => {
+  fs.readdirSync("./C-slash/").forEach((dir) => {
     const commandFiles = fs
       .readdirSync(`./C-slash/${dir}`)
-      .filter(file => file.endsWith(".js"));
+      .filter((file) => file.endsWith(".js"));
 
     for (const file of commandFiles) {
       const command = require(`../C-slash/${dir}/${file}`);
