42513b93e4861b9539582a38be7ff8e13e5be9ca
Glitch (patch-dog-watercress) <none>
2022-01-26 13:21:20 +0000

    🔨🐲 Checkpoint ./C-slash/admin/hama.js:24343700/3073

--- C-slash/admin/hama.js
+++ C-slash/admin/hama.js
@@ -1,22 +1,23 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Color } = require("../../config.js")
+const { Color } = config.embed.Color
 module.exports = {
-data: new SlashCommandBuilder()
-.setName("ga")
-.setDescription("Info about the bot and its creator!")
-  .addSubcommand(subcommand =>
-		subcommand
-			.setName('user')
-			.setDescription('Info about a user')
-			.addUserOption(option => option.setName('target').setDescription('The user'))),
-  memberPermissions: [ "SEND_MESSAGES","MANAGE_GUILD","ADMINISTRATOR" ],			
-  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],		
-  ownerOnly: false,			
+  data: new SlashCommandBuilder()
+    .setName("prefix")
+    .setDescription("setprefix")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("set")
+        .setDescription("setprefix in guild")
+        .addUserOption((option) =>
+          option.setName("prefix").setDescription("The user")
+        )
+    ),
+  memberPermissions: ["SEND_MESSAGES", "MANAGE_GUILD", "ADMINISTRATOR"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  enable: true,
   cooldown: 10000,
-prime: false,
-  run: async (bot, message, args, dev) => {
-    
-    
-  }}
-
+  prime: false,
+  run: async (bot, message, args, dev) => {},
+};
