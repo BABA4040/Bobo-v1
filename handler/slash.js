5fabc98af2e50eccb1f684c3e6978ac3e709f7e9
Glitch (patch-dog-watercress) <none>
2022-01-26 13:09:18 +0000

    ☎️🎗 Checkpoint ./handler/slash.js:24343700/31

--- handler/slash.js
+++ handler/slash.js
@@ -12,7 +12,7 @@ const guild = "850129663896190996";
 module.exports = async (bot) => {
   //////resting slash command
   const commands = [];
-
+bot.once("ready",async()=>{
   try {
     console.log("Started refreshing application (/) commands.");
 
@@ -33,7 +33,7 @@ module.exports = async (bot) => {
     }
   } catch (error) {
     console.log(error);
-  }
+  }})
 
   fs.readdirSync("./C-slash/").forEach((dir) => {
     const commandFiles = fs
