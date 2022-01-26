8a8b0060bf7efbfcf2dc4312ae4cfb9a44f63ec9
Glitch (patch-dog-watercress) <none>
2022-01-26 14:09:21 +0000

    💎🐔 Checkpoint ./C-slash/admin/hama.js:24343700/2620 ./commands/admin/xp.js:24343700/2054

--- C-slash/admin/hama.js
+++ C-slash/admin/hama.js
@@ -3,25 +3,36 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Color } = config.embed.Color;
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("xp toggle")
+    .setName("xp")
     .setDescription("xptoggle")
-   .addSubcommand((subcommand) =>
-      subcommand
-        .setName("xp)
-        .setDescription("to on xp in server")
-        .addStringOption((option) =>
-          option.setName("xp").setDescription("on or of")
-        )
-    ),
+  .addStringOption(option => option.setName("toggle"). setDescription("xpon")),
   memberPermissions: ["SEND_MESSAGES", "MANAGE_GUILD"],
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   ownerOnly: false,
-  enable: true,
+  enabled: true,
   cooldown: 10000,
   prime: false,
-  run: async (bot, message, args, dev) => {
+  run: async (bot, interaction) => {
+    
+    let guild = await Guild.findOne({guildID: interaction.guildId});
+     
+    if ( "on") {
+      guild.xp.onoff = "on";
+      guild.save();
+      return interaction.reply({content:`xp status has been update to **on**`})
+    
+     } else if ("off") {
+        guild.xp.onoff = "off";
+        guild.save();
+      return interaction.reply({content:` Xp system from guild is disabled`})
+    }
+  
+      return interaction.reply({content:`error syntax [on,off] `})
 
-  
-  
+    
+    
+    
+    
+    
   },
 };
