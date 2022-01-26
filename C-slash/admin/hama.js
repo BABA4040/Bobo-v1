d73e4125e5c8ebacb487483e3c722669f56bf57d
Glitch (patch-dog-watercress) <none>
2022-01-26 13:45:20 +0000

    ☺️🐽 Checkpoint ./C-slash/admin/hama.js:24343700/1885

--- C-slash/admin/hama.js
+++ C-slash/admin/hama.js
@@ -1,16 +1,18 @@
 const Discord = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Color } = config.embed.Color
+const { Color } = config.embed.Color;
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("prefix")
     .setDescription("setprefix")
-    .addSubcommand((subcommand) =>
+
+
+   .addSubcommand((subcommand) =>
       subcommand
         .setName("set")
         .setDescription("setprefix in guild")
         .addUserOption((option) =>
-          option.setName("prefix").setDescription("The user")
+          option.setName("input").setDescription("The user")
         )
     ),
   memberPermissions: ["SEND_MESSAGES", "MANAGE_GUILD", "ADMINISTRATOR"],
