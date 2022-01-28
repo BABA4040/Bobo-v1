d9fbf78fdedea5fa7ff5ba3f0e944e85c8956eff
Glitch (patch-dog-watercress) <none>
2022-01-28 23:13:53 +0000

    Import of lordm121/W-es

--- data/maintenance.js
+++
@@ -1,9 +0,0 @@
-
-const maintenance = mongoose.Schema({
-
-    maintenance: {type: String, default: 'maintenance'},
-    toggle: {type: String, default: 'false'},
-   
-})
-
-module.exports = mongoose.model('maintenance', maintenance)
--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports= class{
-  async run(bot){
-  bot.user.setActivity("Bohelp", { type: "WATCHING" });
-    console.log(`${bot.user.username}: registered`)
-}}
