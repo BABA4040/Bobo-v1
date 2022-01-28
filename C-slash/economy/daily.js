c8e522ae9b35e7f0fcdd2f1338509472e6d4d71a
Glitch (patch-dog-watercress) <none>
2022-01-28 10:15:09 +0000

    🏐💷 Checkpoint ./C-slash/economy/daily.js:24343700/724

---
+++ C-slash/economy/daily.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Color } = require("../../config.js")
+module.exports = {
+data: new SlashCommandBuilder()
+.setName("daily")
+.setDescription("get your daily all 24 hours"),
+  enabled: true,			    
+  memberPermissions: [ "SEND_MESSAGES" ],			
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],		
+  enabled:true,
+  category:["general"],
+  ownerOnly: false,			
+  cooldown: 10000,
+prime: false,
+  run: async (interaction,bot,data) => {
+
+    
+  }}
